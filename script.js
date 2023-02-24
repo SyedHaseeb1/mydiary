fb3fb87fc05edb8c7397e2b7646b1fba4bbcb9e4
syedhaseeb1 <haseebumerzai@gmail.com>
2023-02-24 22:38:35 +0500

    indentation

--- script.js
+++ script.js
@@ -3,42 +3,84 @@ const entriesDiv = document.querySelector('#entries');
 let entries = JSON.parse(localStorage.getItem('diaryEntries')) || [];
 
 function saveEntryToLocalStorage(title, date, content) {
-	entries.push({ title, date, content });
-	localStorage.setItem('diaryEntries', JSON.stringify(entries));
+  entries.push({ title, date, content });
+  localStorage.setItem('diaryEntries', JSON.stringify(entries));
 }
 
 function loadEntriesFromLocalStorage() {
 	entries.forEach((entry) => {
-		const entryDiv = document.createElement('div');
-		entryDiv.classList.add('entry');
-		entryDiv.innerHTML = `
-			<h2>${entry.title}</h2>
-			<p>${entry.date}</p>
-			<div class="content">${entry.content}</div>
-		`;
-		entriesDiv.prepend(entryDiv);
+	  const entryDiv = document.createElement('div');
+	  entryDiv.classList.add('entry');
+	  entryDiv.dataset.id = entry.id;
+  
+	  entryDiv.innerHTML = `
+	  <html>
+	  <div class="entry-header">
+		<h2>${entry.title}</h2>
+		<button class="delete-button">&times;</button>
+	  </div>
+	  <p>${entry.date}</p>
+	  <div style="white-space: pre-wrap;">${entry.content}</div>
+
+	</html>`;
+	
+
+	  const deleteButton = entryDiv.querySelector('.delete-button');
+	  deleteButton.addEventListener('click', () => {
+		const confirmed = confirm('Are you sure you want to delete this entry?');
+		if (confirmed) {
+		  entries = entries.filter((e) => e.title !== entry.title);
+		  localStorage.setItem('diaryEntries', JSON.stringify(entries));
+		  entryDiv.remove();
+		}
+	  });	  
+	  entriesDiv.prepend(entryDiv);
 	});
-}
+  }
+  
+
+  
 
 form.addEventListener('submit', (e) => {
-	e.preventDefault();
+  e.preventDefault();
 
-	const title = form.title.value;
-	const date = form.date.value;
-	const content = form.content.value;
+  const title = form.title.value;
+  const date = form.date.value;
+  const content = form.content.value;
 
-	const entryDiv = document.createElement('div');
-	entryDiv.classList.add('entry');
-	entryDiv.innerHTML = `
-		<h2>${title}</h2>
-		<p>${date}</p>
-		<div class="content">${content}</div>
-	`;
-	entriesDiv.prepend(entryDiv);
+  const entryDiv = document.createElement('div');
+  entryDiv.classList.add('entry');
+  entryDiv.innerHTML = `
+	<h2>${title}</h2>
+	<p>${date}</p>
+	<div style="white-space: pre-wrap;">${content}</div>
+	<button class="delete" data-index="${entries.length}">X</button>
+  `;
+  entriesDiv.prepend(entryDiv);
 
-	saveEntryToLocalStorage(title, date, content);
+  saveEntryToLocalStorage(title, date, content);
 
-	form.reset();
+  form.reset();
+});
+
+entriesDiv.addEventListener('click', (e) => {
+  if (e.target.classList.contains('delete')) {
+	const index = e.target.dataset.index;
+	e.target.parentElement.remove();
+	deleteEntryFromLocalStorage(index);
+  }
 });
 
 loadEntriesFromLocalStorage();
+
+// Get today's date in yyyy-mm-dd format
+const today = new Date().toISOString().split('T')[0];
+// Set the date input's value to today's date
+document.getElementById('date').value = today;
+
+const dateLabel = document.getElementById('date-label');
+dateLabel.addEventListener('click', () => {
+  const dateField = document.getElementById('date');
+  dateField.click();
+});
+	
